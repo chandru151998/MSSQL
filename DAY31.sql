51e4ad70c2f18503cb046e134af071c2a31dd14b
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-09-08 12:44:14 +0530

    Employee payroll service

--- DAY31.sql
+++ DAY31.sql
@@ -12,31 +12,31 @@ startDate Date
 );
 
 /* UC3 */
-Insert into employee_payroll values('Shreya',70000,GETDATE());
-Insert into employee_payroll values('Chandru',80000,GETDATE());
-Insert into employee_payroll values('Vetri',60000,GETDATE());
-Insert into employee_payroll values('Srikant',180000,GETDATE());
-Insert into employee_payroll values('Ajay',100000,'2018-01-01');
+Insert into employee_payroll values('Vaishnavi',70000,GETDATE());
+Insert into employee_payroll values('Gourav',80000,GETDATE());
+Insert into employee_payroll values('Vishal',60000,GETDATE());
+Insert into employee_payroll values('Vaibhav',180000,GETDATE());
+Insert into employee_payroll values('Vinayak',100000,'2018-01-01');
 
 /* UC4 */
 Select * from employee_payroll;
 
 /* UC5 */
-SELECT salary FROM employee_payroll WHERE name = 'Vetri';
+SELECT salary FROM employee_payroll WHERE name = 'Vishal';
 
 SELECT salary FROM employee_payroll WHERE startDate BETWEEN CAST('2018-01-01' AS DATE) AND GETDATE();
 
 /* UC6 */
 ALTER table employee_payroll Add  Gender char ;
-UPDATE employee_payroll SET Gender = 'F' where name = 'Shreya';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Chandru';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Vetri';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Srikant';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Ajay';
+UPDATE employee_payroll SET Gender = 'F' where name = 'Vaishnavi';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Gourav';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vishal';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vaibhav';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vinayak';
 Select * from employee_payroll;
 
 /* UC7 */
-INSERT INTO employee_payroll values (6,'Joe',60000,GETDATE(),'F');
+INSERT INTO employee_payroll values (6,'Arti',60000,GETDATE(),'F');
 Select * from employee_payroll;
 
 SELECT SUM(salary) FROM employee_payroll WHERE Gender = 'F' GROUP BY Gender;
