634da387e51f069e0214b9deb3fe4858aa37313f
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-09-08 12:18:30 +0530

    Address book sql with ER diagram

--- Addressbook.sql
+++ Addressbook.sql
@@ -98,3 +98,87 @@ INSERT INTO AddressBook VALUES('Ankita','Sonwane', 'Sarthi', 'Pune','Maharashtra
 INSERT INTO AddressBook VALUES('Vetri','Bandger', 'Shanti Niwas', 'Ichalkaranji','Maharashtra',951423,9516284375,'vetri485@gmail.com', 'Friend');
 INSERT INTO AddressBook VALUES('Girish','Kusgal', 'Om Niwas', 'Ranchi','Zarkhand',852643,7531598464,'girish9@gmail.com', 'Professional');
 SELECT * FROM AddressBook;
+
+/* UC 12 */
+create table AddressBookTable
+(
+AddressBookID int identity(1,1) primary key,
+AddressBookName varchar(255)
+);
+
+INSERT INTO AddressBookTable values
+('Engineers'),('Doctors');
+
+SELECT * FROM AddressBookTable;
+
+create table PersonType
+(
+PersonID int identity(1,1) primary key,
+PersonType varchar(255)
+);
+
+INSERT INTO PersonType values
+('Family'),('Friend'),('Profession');
+
+SELECT * FROM PersonType;
+
+create table Person
+(
+PersonID int identity(1,1) primary key,
+AddressBookID int,
+PersonType varchar(255),
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+Zipcode int,
+PhoneNumber bigint,
+Email varchar(256)
+foreign key (AddressBookID) references AddressBookTable(AddressBookID)
+);
+
+INSERT INTO Person values
+(1,'Friend','Vetri','V','Hosur','Hosur','TN',124563,1234567892,'vetri1542@gmail.com'),
+(2,'Family','Srikant','S','Vijayawada','Vijayawada','Andhra',548692,9865324785,'srikant2@gmail.com'),
+(1,'Professional','Aishwarya','K','Hydrabad','Hydrabad','TN',954862,15984263,'aishwaryaK@gmail.com');
+
+SELECT * FROM Person;
+
+create table MapPersonType
+(
+PersonID int foreign key references Person(PersonID),
+PersonTypeID int foreign key references PersonType(PersonID)
+);
+
+INSERT INTO MapPersonType values (1,1),(2,2),(3,3),(2,1);
+
+SELECT * FROM MapPersonType;
+
+/* UC 13 */
+/* UC 6 */
+select p.PersonID,concat(p.FirstName,p.LastName)as Name,concat(p.Address,',',p.City,',',p.State,',',p.ZipCode) as Address,
+p.PhoneNumber,p.Email,pt.PersonID,pt.PersonType,ab.AddressBookID ,ab.AddressBookName
+from AddressBookTable ab,Person p,PersonType pt,MapPersonType mpt
+where (ab.AddressBookID=p.AddressBookID  and p.PersonID=mpt.PersonID and mpt.PersonTypeID=pt.PersonID) and (City='Hosur' or State='TN') ;
+
+/* UC 7 */
+select count(*),State from Person 
+inner join AddressBookTable on Person.AddressBookID= AddressBookTable.AddressBookID group by State;
+
+select count(*),City
+from Person 
+inner join AddressBookTable on Person.AddressBookID= AddressBookTable.AddressBookID group by City;
+
+/* UC 8*/
+select p.PersonID,concat(p.FirstName,p.LastName)as Name,
+concat(p.Address,',',p.City,',',p.State,',',p.ZipCode) as Address,p.PhoneNumber,p.Email,
+pt.PersonID,pt.PersonType,ab.AddressBookID ,ab.AddressBookName
+from AddressBookTable ab,Person p,PersonType pt,MapPersonType mpt
+where (ab.AddressBookID=p.AddressBookID  and p.PersonID=mpt.PersonID and mpt.PersonID=pt.PersonID) and (State='TN') order by FirstName;
+
+/* UC 10 */
+SELECT COUNT(*),pt.PersonType FROM
+MapPersonType AS mpt 
+inner join PersonType as pt On pt.PersonId = mpt.PersonId
+inner join Person as p on p.PersonId = mpt.PersonId GROUP BY pt.PersonType;
